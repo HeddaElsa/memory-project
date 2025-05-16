96b3b3a6eb25b38df29c200f3bbb0c22e45c9ec2
Luca Pirro <aspudds-luca@hotmail.com>
2025-05-16 13:22:55 +0200

    cleanup H

--- our-react-app/src/components/FetchandShuffle.jsx
+++ our-react-app/src/components/FetchandShuffle.jsx
@@ -13,7 +13,6 @@ const FetchAndShuffle = () => {
             const response = await fetch ("https://dog.ceo/api/breeds/image/random/4")
             const data = await response.json();
             setDogImages(data.message.map((fetchedImage) => ({src: fetchedImage}))); 
-            console.log("Images from API", data.message);
         } catch (error) {
             console.error("Error fetching data:", error);
         }
@@ -21,26 +20,23 @@ const FetchAndShuffle = () => {
 
     useEffect (() => {fetchData()} , []);
 
-    const ShuffleCards = () => {
+    const shuffleCards = () => {
 
-        const Shuffled = [...dogImages, ...dogImages]
+        const shuffled = [...dogImages, ...dogImages]
             .sort(() => Math.random() - 0.5)
             .map((dogImages) => ({
                     id: Math.random(),
                     src: dogImages.src,
             }));
-        setCards(Shuffled);
+        setCards(shuffled);
         setTurns(0);
-        console.log("Mixed cards shuffled", Shuffled);
-
     };
-
-    console.log("Cards array:", cards);
+    console.log("Cards array:", cards, turns);
 
         return (
         <div className="flex items-center justify-center h-screen bg-black">
         <div>
-            <button onClick={ShuffleCards} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Shuffle</button>
+            <button onClick={shuffleCards} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Shuffle</button>
         </div>
 
         <div className="card-grid">
--- our-react-app/src/components/card.jsx
+++ our-react-app/src/components/card.jsx
@@ -4,14 +4,14 @@ import "./card.css";
 function Card({card}) {
   
     return (
-    <div>
+    
        <div className="card">
-            <div className="card">
+            <div>
                 <img src={card.src} alt="card.front" className="dog-image"/>
                 <img src="/images/questiondog5.jpg" alt="card.back" className="back-image"/>
             </div>
         </div>  
-    </div>
+
     )
   }
   
